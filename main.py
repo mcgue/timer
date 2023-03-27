d9e8f78d784e6c2d46b560b47765a4f26adfc0c3
mcgue <mcgu@protonmail.com>
2023-03-27 14:25:16 -0700

    add more code to start

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # Countdown timer
+
+import time
+
+# Get user input on how long time last
+
+length = float(input('How long do you want the timer for? '))
+
+# Verify is a number
+
+
+print(length)
