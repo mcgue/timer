59602a4e30b264a20030547cea3d1ae1a56dff04
mcgue <mcgu@protonmail.com>
2023-03-30 09:34:54 -0700

    add code to print timer

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def countdown(length):
   length = int(length)
   # Get minutes and seconds
   mins, secs = divmod(length, 60)
-  print(mins, secs)
+  timer = f'{mins}:{secs}'
+  print(timer)
 
 # Get user input on length of timer
 length = input('How many seconds do you want to count down from? ')
