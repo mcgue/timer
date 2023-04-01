ce78332401178d529da8c9eae00237382e0fd539
mcgue <mcgu@protonmail.com>
2023-03-31 19:43:44 -0700

    add while loop to do countdown

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import time
 
 # Function to do countdown
 def countdown(length):
+  # Convert length to integer
   length = int(length)
-  # Get minutes and seconds
-  mins, secs = divmod(length, 60)
-  timer = f'{mins}:{secs}'
-  print(timer)
+  # Loop to countdown to 0
+  while length:
+    # Get minutes and seconds
+    mins, secs = divmod(length, 60)
+    timer = f'{mins}:{secs}'
+    print(timer)
+    length -= 1
+  print ('Zero')
 
 # Get user input on length of timer
 length = input('How many seconds do you want to count down from? ')
